35dad09c2f4e777fbec66da1c4a886dc35344cdb
Dante Camarena <hello@mhdante.com>
2021-08-17 14:19:48 -0400

    normalzie task and uri across platforms

--- UnityProject/Packages/ai.transforms.grpc-web-unity/Runtime/Api/GrpcWebUnity.cs
+++ UnityProject/Packages/ai.transforms.grpc-web-unity/Runtime/Api/GrpcWebUnity.cs
@@ -3,33 +3,41 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Grpc.Core;
+using UnityEngine;
 
 namespace GrpcWebUnity
 {
     public static class GrpcWeb
     {
-        private static string[] ValidSchemes { get; } = {Uri.UriSchemeHttps, Uri.UriSchemeHttp};
+        private static string[] ValidSchemes { get; } = { Uri.UriSchemeHttps, Uri.UriSchemeHttp };
 
         public static async Task<ChannelBase> GetChannelAsync(string targetAddress,
             ChannelCredentials credentials = null, CancellationToken cancellationToken = default)
         {
             credentials ??= ChannelCredentials.Insecure;
 
-            bool valid = Uri.TryCreate(targetAddress, UriKind.Absolute, out var uri);
-            if (!valid || !ValidSchemes.Contains(uri.Scheme))
-                valid = Uri.TryCreate("http://" + targetAddress, UriKind.Absolute, out uri);
+            // Added in if rather than compile defines to avoid accidental refactoring from breaking it.
+            if (Application.platform == RuntimePlatform.WebGLPlayer)
+            {
+                bool valid = Uri.TryCreate(targetAddress, UriKind.Absolute, out var uri);
+                if (!valid || !ValidSchemes.Contains(uri.Scheme))
+                    valid = Uri.TryCreate("http://" + targetAddress, UriKind.Absolute, out uri);
 
-            if (!valid) throw new UriFormatException($"{nameof(targetAddress)} is not a valid URI: {targetAddress}");
+                if (!valid) throw new UriFormatException($"{nameof(targetAddress)} is not a valid URI: {targetAddress}");
 
-            targetAddress = uri.ToString();
-            
-            
+                targetAddress = uri.ToString();
+
+            }
+
+
 
 #if UNITY_WEBGL && !UNITY_EDITOR
-            var instance = GrpcWebUnity.Internal.GrpcWebConnector.Instance;
+            var instance = Internal.GrpcWebConnector.Instance;
             await instance.WaitForInitialization.WithCancellation(cancellationToken);
             return instance.MakeChannel(targetAddress);
 #else
+            // Ensures consistent behaviour across platforms 
+            await Task.Delay(1, cancellationToken);
             return new Channel(targetAddress, credentials);
 #endif
         }
