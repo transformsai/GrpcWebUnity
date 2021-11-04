1205cfab8ed2153127da7afca6b0aa353cab177c
Dante Camarena <hello@mhdante.com>
2021-11-04 16:52:42 -0400

    Parse out credentials value for given addresses

--- UnityProject/Packages/ai.transforms.grpc-web-unity/Runtime/Api/GrpcWebUnity.cs
+++ UnityProject/Packages/ai.transforms.grpc-web-unity/Runtime/Api/GrpcWebUnity.cs
@@ -11,32 +11,53 @@ namespace GrpcWebUnity
     {
         private static string[] ValidSchemes { get; } = { Uri.UriSchemeHttps, Uri.UriSchemeHttp };
 
+        /// <summary>
+        /// Gets a GRPC channel depending on the current implementation.
+        /// On WebGL, it will return a webgl channel. On standalone, it will return a default GRPC channel.
+        /// If no scheme is given, it will check the credentials or default to insecure http.
+        /// If no port is given, will default to 80 for insecure and 443 for secure.
+        /// Call is async because waiting for a channel on WebGL requires operations in javascript to occur
+        /// outside of the current unity frame.
+        /// </summary>
+        /// <param name="targetAddress">
+        /// The target to connect to.
+        /// </param>
+        /// <param name="credentials">Optional. if not set, will check <paramref name="targetAddress"/> scheme</param>
+        /// <param name="cancellationToken">used to cancel waiting for the connection. </param>
+        /// <returns></returns>
         public static async Task<ChannelBase> GetChannelAsync(string targetAddress,
             ChannelCredentials credentials = null, CancellationToken cancellationToken = default)
         {
-            credentials ??= ChannelCredentials.Insecure;
-
-            // Added in if rather than compile defines to avoid accidental refactoring from breaking it.
-            if (Application.platform == RuntimePlatform.WebGLPlayer)
+            if(credentials == null)
             {
-                bool valid = Uri.TryCreate(targetAddress, UriKind.Absolute, out var uri);
-                if (!valid || !ValidSchemes.Contains(uri.Scheme))
-                    valid = Uri.TryCreate("http://" + targetAddress, UriKind.Absolute, out uri);
-
-                if (!valid) throw new UriFormatException($"{nameof(targetAddress)} is not a valid URI: {targetAddress}");
-
-                targetAddress = uri.ToString();
-
+                if (targetAddress.StartsWith(Uri.UriSchemeHttps)) credentials = ChannelCredentials.SecureSsl;
+                else credentials = ChannelCredentials.Insecure;
             }
+
+            bool valid = Uri.TryCreate(targetAddress, UriKind.Absolute, out var uri);
+
+            if (!valid || !ValidSchemes.Contains(uri.Scheme))
+            {
+                var scheme = "http://";
+                if (credentials == ChannelCredentials.SecureSsl) scheme = "https://";
+                valid = Uri.TryCreate(scheme + targetAddress, UriKind.Absolute, out uri);
+            }
+
+            if (!valid) throw new UriFormatException($"{nameof(targetAddress)} is not a valid URI: {targetAddress}");
+
+            string webTargetAddress = uri.ToString().TrimEnd('/');
+            // Removes scheme (required for standalone)
+            string standaloneTargetAddress = uri.GetComponents(UriComponents.AbsoluteUri & ~UriComponents.Scheme, UriFormat.UriEscaped).TrimEnd('/');
+
 
 #if UNITY_WEBGL && !UNITY_EDITOR
             var instance = Internal.GrpcWebConnector.Instance;
             await instance.WaitForInitialization.WithCancellation(cancellationToken);
-            return instance.MakeChannel(targetAddress);
+            return instance.MakeChannel(webTargetAddress);
 #else
-            // Ensures consistent behaviour across platforms 
+            // Delay ensures consistent behaviour across platforms 
             await Task.Delay(1, cancellationToken);
-            return new Channel(targetAddress, credentials);
+            return new Channel(standaloneTargetAddress, credentials);
 #endif
         }
 
